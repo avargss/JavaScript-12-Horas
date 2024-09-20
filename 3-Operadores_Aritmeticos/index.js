603bb3b6163d9086475d762fe50f7fd8674fe19b
Alejandro Vargas Garrán <alevargasgarran@gmail.com>
2024-09-20 21:54:08 +0200

    Operadores

---
+++ 3-Operadores_Aritmeticos/index.js
@@ -0,0 +1,21 @@
+// Operadores aritméticos = Operandos (Valores, variables, etc.)
+//                          Operadores (+, -, *, /)
+//                          Ej. 11 = x + 5;
+
+let estudiantes = 30;
+
+// estudiantes = estudiantes + 1;
+// estudiantes = estudiantes - 1;
+// estudiantes = estudiantes * 2;
+// estudiantes = estudiantes / 2;
+// estudiantes = estudiantes ** 3; Dos asteriscos significan elevado a.
+// let extraEstudiantes = estudiantes % 2;
+
+// estudiantes += 1;
+// estudiantes -= 1;
+// estudiantes *= 2;
+// estudiantes /= 2;
+// estudiantes ** 2;
+// estudiantes %= 2 ;
+
+console.log(estudiantes);
